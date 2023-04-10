453a0d670dae36d105589c4288d4d10196b7696f
Emerson Lima <emersonlimago123@gmail.com>
2023-04-10 13:10:49 -0300

    added main offer desc

--- reactapp/src/pages/Home/Home.jsx
+++ reactapp/src/pages/Home/Home.jsx
@@ -8,7 +8,16 @@ function Home(){
         <Header />
         <main className="main-section">
             <div className="main-offer">
-           
+                <div className='offer-content'>
+                    <div className='offer-title'>
+                        <h2>Tênis Esportivo Para Corrida Top</h2>
+                    </div>
+                <p className='product-description'>
+                Esse tênis tem o intuito de te deixar ainda mais fitness, focado e musculoso
+                </p>
+                <p className='price'>R$ 1.000,00</p>
+                <button>COMPRAR</button>
+                 </div>
             </div>
             <div className="offer-images">
 
